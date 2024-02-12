aa99ea443872742d8c333c900ddf3724b9cd809a
pavel <p@b.ru>
2024-02-13 01:07:24 +0200

    delete host and port

--- app/config.py
+++ app/config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 class Settings:
     load_dotenv()
-    port=int(os.getenv("PORT"))
-    host=os.getenv("HOST")
     token=os.getenv("TOKEN")
-    
+
 settings=Settings()
